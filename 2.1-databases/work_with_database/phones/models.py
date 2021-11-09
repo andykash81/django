7c2c52dfe0058795763e2baa60cd4d8009814128
andykash81 <k81@ngs.ru>
2021-11-09 22:09:39 +0700

    Выполнение задания "Выгрузка каталога товаров из csv-файла"

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from slugify import slugify
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField(upload_to='res/')
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f'iphone {self.name}')
+        super(Phone, self).save(*args, **kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,26 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    request_sort = request.GET.get('sort')
+    if request_sort == 'name':
+        sort = phones.order_by('name')
+    elif request_sort == 'min_price':
+        sort = phones.order_by('price')
+    elif request_sort == 'max_price':
+        sort = phones.order_by('-price')
+    else:
+        sort = phones
+    context = {
+        'phones': sort
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug).values()
+    context = {
+        'phone': phone[0]
+    }
     return render(request, template, context)
